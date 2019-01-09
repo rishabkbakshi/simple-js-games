912182b069dd889feb3a813a0afeee26dd5b4c3e
Rishab Kumar Bakshi <rishabkbakshi@gmail.com>
2019-01-08 18:19:39 -0600

    Added bug fixes and error handling for hangman

--- games/hangman/hangman.js
+++ games/hangman/hangman.js
@@ -2,6 +2,7 @@
 let HANGMAN_WORD = "";
 let CURRENT_TRY = 1;
 let userInputArr = [];
+let incorrectLetterArr = [];
 
 const WORDS_LIST = [
     'india', 'germany', 'nepal', 'oman', 'france',
@@ -13,20 +14,43 @@ const WORDS_LIST = [
 const generateWord = () => {
     const randomIndex = Math.floor(Math.random() * Math.floor(WORDS_LIST.length));
     HANGMAN_WORD = WORDS_LIST[randomIndex];
-    console.log(HANGMAN_WORD);
+    // console.log(HANGMAN_WORD);
 }
 
 const letterBox = document.getElementById("letterBox");
-const hangmanCanvas = document.getElementById("hangmanCanvas");
+let hangmanCanvas = document.getElementById("hangmanCanvas");
+let context = hangmanCanvas.getContext('2d')
 const incorrectBox = document.getElementById("incorrectBox");
+const userInputBox = document.getElementById("userInput"); 
 
 const canvas_width = hangmanCanvas.width;
 const canvas_height = hangmanCanvas.height;
 
-const onUserInput = (e) => {
-    const userInput = e.target.value;
-    (userInput.length > 1) ? alert("Please enter one letter") : (userInputArr.indexOf(userInput) > -1) ? alert("You already entered this letter!") : evaluateInput(userInput)
-    e.target.value = "";
+const startNewGame = () => {
+    generateWord();
+    initializeBlanks();
+    CURRENT_TRY = 1;
+    clearCanvas();
+    incorrectBox.innerHTML = '';
+    userInputArr = [];
+    incorrectLetterArr = [];
+}
+
+document.querySelector('#userInput').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+      onUserInput()
+    }
+});
+
+const onUserInput = () => {
+    const userInput = userInputBox.value.toLowerCase();
+    (userInput.length > 1) ? alert("Please enter one letter")
+     : (userInputArr.indexOf(userInput) > -1) ? alert("You already entered this letter!")
+      : (incorrectLetterArr.indexOf(userInput) > -1) ? alert("You already entered this.")
+       : evaluateInput(userInput)
+    userInputBox.value = "";
+    userInputBox.focus();
 }
 
 const evaluateInput = (inputLetter) => {
@@ -42,7 +66,6 @@ const findIndices = (inputString, inputLetter) => {
             indices.push(index);
         }
     });
-    console.log(inputLetter, indices);
     return indices;
 }
 
@@ -57,16 +80,14 @@ const hangmanIsDead = () => {
 }
 
 const placeLetter = (inputLetter, letterIndices) => {
-    userInputArr.push(inputLetter);
     let blankArr = Array.from(letterBox.childNodes);
-    console.log(letterIndices)
     letterIndices.forEach((letterIndex, i) => {
-        console.log(letterIndex);
         blankArr.forEach((blank, j) => {
             if (letterIndex === j) {
-                console.log(letterIndex, j);
                 blankArr[j].innerHTML = inputLetter;
-                if(userInputArr.length == HANGMAN_WORD.length){
+                userInputArr.push(inputLetter);
+                if (userInputArr.length == HANGMAN_WORD.length) {
+
                     gameIsWon();
                 }
             }
@@ -78,55 +99,58 @@ const addToIncorrectBox = (incorrectLetter) => {
     incorrectBox.innerHTML += incorrectLetter + ', ';
 }
 
-const drawHangman = (inputLetter) => {
-    console.log("Hangman");
-    switch (CURRENT_TRY) {
-        case 1:
-            drawLineParts(0.25 * canvas_width, 0.05 * canvas_height, 0.75 * canvas_width, 0.05 * canvas_height)
-            break;
-        case 2:
-            drawLineParts(0.5 * canvas_width, 0.05 * canvas_height, 0.5 * canvas_width, 0.25 * canvas_height)
-            break;
-        case 3:
-            drawHead();
-            break;
-        case 4:
-            drawLineParts(0.5 * canvas_width, 0.45 * canvas_height, 0.35 * canvas_width, 0.65 * canvas_height);
-            break;
-        case 5:
-            drawLineParts(0.5 * canvas_width, 0.45 * canvas_height, 0.65 * canvas_width, 0.65 * canvas_height);
-            break;
-        case 6:
-            drawLineParts(0.5 * canvas_width, 0.45 * canvas_height, 0.5 * canvas_width, 0.75 * canvas_height);
-            break;
-        case 7:
-            drawLineParts(0.5 * canvas_width, 0.75 * canvas_height, 0.35 * canvas_width, 0.95 * canvas_height);
-            break;
-        case 8:
-            drawLineParts(0.5 * canvas_width, 0.75 * canvas_height, 0.65 * canvas_width, 0.95 * canvas_height);
-            setTimeout(hangmanIsDead,1000);
-            break;
-        default:
-            hangmanIsDead();
-            console.log("game over");
-    }
-    CURRENT_TRY++;
-    addToIncorrectBox(inputLetter);
-}
-
 const drawLineParts = (startX, startY, endX, endY) => {
-    const ctx = hangmanCanvas.getContext("2d");
-    ctx.moveTo(startX, startY);
-    ctx.lineTo(endX, endY);
-    ctx.stroke();
+    // const context = hangmanCanvas.getContext("2d");
+    context.moveTo(startX, startY);
+    context.lineTo(endX, endY);
+    context.stroke();
 }
 
 const drawHead = () => {
-    const ctx = hangmanCanvas.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(0.5 * canvas_width, 0.35 * canvas_height, (0.10 * canvas_height), 0, 2 * Math.PI);
-    ctx.stroke();
+    // const context = hangmanCanvas.getContext("2d");
+    context.beginPath();
+    context.arc(0.5 * canvas_width, 0.35 * canvas_height, (0.10 * canvas_height), 0, 2 * Math.PI);
+    context.stroke();
 }
+
+
+const drawHangman = (inputLetter) => {
+    console.log("Hangman", CURRENT_TRY);
+    switch (CURRENT_TRY) {
+        case 1:
+            drawHead(); //Head
+            break;
+        case 2:
+            drawLineParts(0.5 * canvas_width, 0.45 * canvas_height, 0.5 * canvas_width, 0.75 * canvas_height); //body
+            break;
+        case 3:
+            drawLineParts(0.5 * canvas_width, 0.45 * canvas_height, 0.35 * canvas_width, 0.65 * canvas_height); // left hand
+            break;
+        case 4:
+            drawLineParts(0.5 * canvas_width, 0.45 * canvas_height, 0.65 * canvas_width, 0.65 * canvas_height); // right hand
+            break;
+        case 5:
+            drawLineParts(0.5 * canvas_width, 0.75 * canvas_height, 0.65 * canvas_width, 0.95 * canvas_height); //Right Leg
+            break;
+        case 6:
+            drawLineParts(0.5 * canvas_width, 0.05 * canvas_height, 0.5 * canvas_width, 0.25 * canvas_height) //rope
+            break;
+        case 7:
+            drawLineParts(0.5 * canvas_width, 0.75 * canvas_height, 0.35 * canvas_width, 0.95 * canvas_height); //left leg
+            break;
+        case 8:
+            drawLineParts(0.25 * canvas_width, 0.05 * canvas_height, 0.75 * canvas_width, 0.05 * canvas_height) //wood
+            setTimeout(hangmanIsDead, 500);
+            break;
+        default:
+            hangmanIsDead();
+            break;
+    }
+    CURRENT_TRY++;
+    addToIncorrectBox(inputLetter);
+    incorrectLetterArr.push(inputLetter);
+}
+
 
 const initializeBlanks = () => {
     letterBox.innerHTML = '';
@@ -138,18 +162,12 @@ const initializeBlanks = () => {
 }
 
 const clearCanvas = () => {
-    const context = hangmanCanvas.getContext('2d');
-    context.clearRect(0, 0, canvas_width, canvas_height);
+    context.fillStyle = "white";
+    //   context.fillRect(10, 10, 100, 50);
+    context.fillRect(0, 0, canvas_width, canvas_height);
 }
 
-const startNewGame = () => {
-    generateWord();
-    initializeBlanks();
-    CURRENT_TRY = 1;
-    clearCanvas();
-    incorrectBox.innerHTML = '';
-    userInputArr = [];
-}
+
 
 startNewGame();
 
